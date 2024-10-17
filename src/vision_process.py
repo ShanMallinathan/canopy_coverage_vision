1ed79347928ba829cbf3a5c52bcf9d44dbec0349
Shan <info@cordon.ai>
2024-10-17 17:24:49 +0100

    image warping

--- src/vision_process.py
+++ src/vision_process.py
@@ -9,6 +9,7 @@ class VisionProcess():
         self.img=cv2.imread(image_loc)
         self.gray=cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         self.aruco_dict_class=cv2.aruco.DICT_6X6_50
+        self.marker_centers=[]
 
     def detect_aruco_markers(self):
         """
@@ -23,6 +24,7 @@ class VisionProcess():
         det_params=cv2.aruco.DetectorParameters()
         detector=cv2.aruco.ArucoDetector(aruco_dict, det_params)
         self.corners, self.id, rejected = detector.detectMarkers(self.gray)
+        self.corners=np.squeeze(self.corners)
         
     def compute_marker_center(self):
         """
@@ -36,4 +38,24 @@ class VisionProcess():
         center of each marker
         """
         self.detect_aruco_markers()
-        
+        for i in range(np.shape(self.corners)[0]): #loop for each marker (usually 4)
+            self.marker_centers.append(self.corners[i].mean(axis=0))
+        self.marker_centers_numpy=np.array(self.marker_centers, np.float32)
+
+    def warp_image(self):
+        """
+        Use the corners from the compute markers centers and use perspective transforms and warp the image
+        ---
+        Computes
+        A numpy array representing the warped image
+        """
+        self.compute_marker_center()
+        warped_points=np.array([[827,1169], [0, 1169], [827, 0], [0,0]], np.float32)
+        transform=cv2.getPerspectiveTransform(self.marker_centers_numpy, warped_points)
+        self.warped_img=cv2.warpPerspective(self.img, transform, (827, 1169))
+
+vis=VisionProcess('media/new_aruco.png')
+vis.warp_image()
+cv2.imshow("warped", vis.warped_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
